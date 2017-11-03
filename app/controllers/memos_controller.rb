7111339e9c80605d6f012d3fe1c1e22ef6e583c0
MinGOODdev <mingood92@gmail.com>
2017-11-03 08:24:50 +0000

    destroy

--- app/controllers/memos_controller.rb
+++ app/controllers/memos_controller.rb
@@ -1,15 +1,20 @@
 class MemosController < ApplicationController
+  # 로그인 user 권한 검사
+  # before_action :authenticate_user!, except: [:show, :index]
+  # @memo = Memo.find(params[:id])를 대체하기 위한 코드
+  before_action :set_memo, only: [:show, :edit, :update, :destroy]
+  
   # Create
   def new
-    # form_for를 위해서 선언한 것이라고 생각하자.
+    # form_for에 채우기 위해서 선언.
     @memo = Memo.new
   end
   
   def create
-    @memo = Memo.new
-    @memo.title = params[:memo][:title]
-    @memo.content = params[:memo][:content]
-    @memo.user_id = params[:memo][:user_id]
+    @memo = Memo.new(memo_params)
+    # @memo.title = params[:memo][:title]
+    # @memo.content = params[:memo][:content]
+    # @memo.user_id = params[:memo][:user_id]
     @memo.save
     redirect_to @memo
     # redirect_to memo_path(@memo)와 의미가 같다.
@@ -18,7 +23,7 @@ class MemosController < ApplicationController
   
   # Retrieve
   def show
-    @memo = Memo.find(params[:id])
+    # @memo = Memo.find(params[:id])
   end
   
   def index
@@ -28,15 +33,17 @@ class MemosController < ApplicationController
   
   # Update
   def edit
-    @memo = Memo.find(params[:id])
+    # @memo = Memo.find(params[:id])
   end
   
   def update
-    @memo = Memo.find(params[:id])
-    @memo.title = params[:memo][:title]
-    @memo.content = params[:memo][:content]
-    @memo.user_id = params[:memo][:user_id]
-    @memo.save
+    # @memo = Memo.find(params[:id])
+    # update는 save까지 알아서 처리한다.
+    @memo.update(memo_params)
+    # @memo.title = params[:memo][:title]
+    # @memo.content = params[:memo][:content]
+    # @memo.user_id = params[:memo][:user_id]
+    # @memo.save
     redirect_to @memo
     # redirect_to memo_path(@memo)와 의미가 같다.
     # 그리고 @memo를 파라미터로 넘겨주면 알아서 id를 찾아서 redirect한다.
@@ -44,7 +51,20 @@ class MemosController < ApplicationController
   
   # Destroy
   def destroy
-    
+    # @memo = Memo.find(params[:id])
+    @memo.destroy!
+    redirect_to root_path
   end
+  
+  # 조금 더 세련되게 바꿔보자. (before_action)
+  private
+    def set_memo
+      @memo = Memo.find(params[:id])
+    end
+    
+    def memo_params
+      #Rails 4 Strong Parameter
+      params.require(:memo).permit(:title, :content, :user_id)
+    end
   
 end
